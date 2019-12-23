8e41cfab4d64ae89065c7f03fae3f65d3d42fa86
triamero <4444613+triamero@users.noreply.github.com>
2019-12-23 22:24:21 +0300

    * background tilemap * background image * some ingredient textures

--- src/scenes/game.scene.ts
+++ src/scenes/game.scene.ts
@@ -1,4 +1,5 @@
 import * as Phaser from "phaser";
+import {Db} from "@it/shared";
 
 export class GameScene extends Phaser.Scene {
 
@@ -17,19 +18,32 @@ export class GameScene extends Phaser.Scene {
 
         this._opened = this.cache.obj.get("opened");
 
-        const rect1 = this.add.rectangle(0, 0, 100, 100, 0xff0000);
+        const rect1 = this.add.rectangle(0, 0, 100, 100, 0x0d084c);
 
-        const rect2 = this.add.rectangle(0, 0, 100, 100, 0xff0000);
+        const rect2 = this.add.rectangle(0, 0, 100, 100, 0x0d084c);
 
         this._firstContainer = this.add.container(60, 200, [rect1]);
         this._secondContainer = this.add.container(500, 200, [rect2]);
+
+        this.load.tilemapTiledJSON("game", "assets/alchemy.json");
+        this.load.image("game-tilemap", "assets/tilebag.png");
     }
+
+    create() {
+        const map = this.make.tilemap({key: "game", height: 800, width: 600});
+
+        const tileset = map.addTilesetImage("tilebag", "game-tilemap", 90, 90);
+
+        const background = map.createStaticLayer("background", tileset, 0, 0);
+    }
+
 
     update() {
 
-        let db = this.cache.json.get("db");
+        let db: Db = this.cache.json.get("db");
 
         if (this._firstId > 0) {
+            const first = db.items.find(x => x.id == this._firstId);
             this._firstContainer.add(this.add.image(0, 0, ""));
         } else {
             //this._firstContainer.removeAll(true);
